999efbf3775842023341860998cb9ef5c57c1c2a
Ammon G <gruwella@gmail.com>
2014-01-30 22:32:41 -0700

    GUI Idea One

---
+++ Whisk/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  Whisk
+//
+//  Created by Ammon Gruwell on 1/30/14.
+//  Copyright (c) 2014 Ammon Gruwell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@end
